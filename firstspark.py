0aca717760097c490cbb780a97414a963402da3e
sang <sang@wiserobot.com>
2019-09-17 09:10:32 +0700

    upload firstspark

--- firstspark.py
+++ firstspark.py
@@ -1,5 +1,5 @@
 from pyspark import SparkContext
-logFile = "file:///home/hadoop/spark-2.1.0-bin-hadoop2.7/README.md"  
+logFile = "ml-100k/u.data"  
 sc = SparkContext("local", "first app")
 logData = sc.textFile(logFile).cache()
 numAs = logData.filter(lambda s: 'a' in s).count()
