5b62480659367d612f8b088bc6c67f49ff6c75f4
sang <sang@wiserobot.com>
2019-09-17 21:36:48 +0700

    add file

--- spark-file.py
+++ spark-file.py
@@ -1,6 +1,6 @@
 from pyspark import SparkContext
 from pyspark import SparkFiles
-finddistance = "/home/maria_dev/finddistance.R"
+finddistance = "/user/maria_dev/finddistance.R"
 finddistancename = "finddistance.R"
 sc = SparkContext("local", "SparkFile App")
 sc.addFile(finddistance)
